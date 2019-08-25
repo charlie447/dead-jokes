51f79dfd69a5a4bdda02eece53d05d97d04b07d2
Charlie J Zhang <charlie.j.zhang@pwc.com>
2019-08-25 18:17:17 +0800

    Add navbar and carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
