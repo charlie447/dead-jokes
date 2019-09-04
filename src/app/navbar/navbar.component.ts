65fed63404067f1b09c17747eea2bc8e4611f6df
Charlie J Zhang <charlie.j.zhang@pwc.com>
2019-09-04 13:32:54 +0800

    #6 Add transition effect when scroll to certain position

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  pagePosition = 0;
+  navShrink = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.pagePosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (this.pagePosition > 50) {
+      // console.log(this.pagePosition);
+      this.navShrink = true;
+    } else {
+      this.navShrink = false;
+    }
+  }
 }
