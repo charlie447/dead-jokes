d52836023205bb0427673f0cd42f0e469f86d4e7
Charlie J Zhang <charlie.j.zhang@pwc.com>
2019-09-04 16:20:34 +0800

    #5 Modify styles in Gallery section

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // Vendor
-import { faLaptop, faCircle } from '@fortawesome/free-solid-svg-icons';
+import { faLaptop, faCircle, faRainbow } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,7 @@ import { faLaptop, faCircle } from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent implements OnInit {
   faLaptop = faLaptop;
   faCircle = faCircle;
+  faRainbow = faRainbow;
 
   galleryItems = [
     "https://dummyimage.com/600x400/f3f3f3/000000.jpg",
