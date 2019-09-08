7df1ee1b322647eea64369466c5645656844d9e7
Charlie J Zhang <charlie.j.zhang@pwc.com>
2019-09-08 19:16:10 +0800

    #1 re-write the login card with reference

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGrinTears, faDizzy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  faGrinTears = faGrinTears;
+  faDizzy = faDizzy;
   constructor() { }
 
   ngOnInit() {
--- src/app/routes/login/login.module.ts
+++ src/app/routes/login/login.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FontAwesomeModule
   ],
   declarations: [LoginComponent]
 })
